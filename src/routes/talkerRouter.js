69460492ffb4ee0276b00f0aed534a9d307287a1
gmaralan <gmartinialan@gmail.com>
2023-03-14 15:59:52 -0400

    req 08

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -22,18 +22,32 @@ talkerRouter.get('/', async (req, res) => {
   res.status(200).json(content);
 });
 
+talkerRouter.get(
+  '/search',
+  validateAutorization,
+  async (req, res) => {
+    const { q } = req.query;
+    const currentTalkers = await readFile(itensPath);
+
+    const regex = new RegExp(q);
+    const filteredTalkers = currentTalkers
+      .filter((talker) => regex.test(talker.name));
+
+    return res.status(200).json(filteredTalkers);
+  },
+);
+
 talkerRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   const content = await readFile(itensPath);
-  console.log('cotnent is', content);
+  
   if (!content) {
     throw new Error('Algo deu errado com a requisição');
   }
 
   const talkerObj = content.find((talker) => talker.id === Number(id));
 
-  console.log('there is a talkerObj:', talkerObj);
   if (!talkerObj) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
