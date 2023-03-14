4716bf29372e3c09c0205a87085a0c9ae507bcc8
gmaralan <gmartinialan@gmail.com>
2023-03-14 14:11:04 -0400

    req2

--- src/manageFiles/read.js
+++ src/manageFiles/read.js
@@ -1,12 +1,9 @@
 const fs = require('fs/promises');
 
 const readFile = async (path) => {
-  console.log('diur is:', __dirname);
-
   const info = await fs.readFile(path);
 
   const parsedInfos = JSON.parse(info);
-  console.log(parsedInfos);
 
   return parsedInfos;
 };
--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -6,11 +6,29 @@ const talkerRouter = express.Router();
 
 const itensPath = path.join(__dirname, '../talker.json');
 
-// Req1
 talkerRouter.get('/', async (req, res) => {
   const content = await readFile(itensPath);
 
   res.status(200).json(content);
+});
+
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const content = await readFile(itensPath);
+  console.log('cotnent is', content);
+  if (!content) {
+    throw new Error('Algo deu errado com a requisição');
+  }
+
+  const talkerObj = content.find((talker) => talker.id === Number(id));
+
+  console.log('there is a talkerObj:', talkerObj);
+  if (!talkerObj) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(talkerObj);
 });
 
 module.exports = talkerRouter;
